50e81cae9647b49d795bcbbb28e4a911d96d5ad4
GD Hiruna <hirunaofficial@gmail.com>
2024-11-16 23:43:30 +0530

    Initial commit

---
+++ src/main/java/dev/hiruna/rescuenet/RescuenetApplication.java
@@ -0,0 +1,13 @@
+package dev.hiruna.rescuenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescuenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescuenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hiruna/rescuenet/RescuenetApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hiruna.rescuenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescuenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
