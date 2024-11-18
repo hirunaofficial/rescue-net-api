772c7d3887fe3560ef1e96a9aaab2db2cfc39028
GD Hiruna <hirunaofficial@gmail.com>
2024-11-18 14:56:35 +0530

    feat: Add UserDTO for transferring user data

---
+++ src/main/java/dev/hiruna/rescuenet/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package dev.hiruna.rescuenet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private String firstName;
+    private String lastName;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
